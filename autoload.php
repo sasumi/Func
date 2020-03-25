51bb3ba19696033f2eb5d82c7afd32250cd8d75d
sasumi <cnsasumi@outlook.com>
2020-03-25 09:06:50 +0800

    rn

--- autoload.php
+++ autoload.php
@@ -2,6 +2,6 @@
 require_once __DIR__.'/src/array.php';
 require_once __DIR__.'/src/env.php';
 require_once __DIR__.'/src/file.php';
-require_once __DIR__.'/src/function.php';
+require_once __DIR__.'/src/util.php';
 require_once __DIR__.'/src/string.php';
 require_once __DIR__.'/src/time.php';
