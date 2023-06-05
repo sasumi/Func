d3494915bca055cf45e692734a8e8df548f951ce
sasumi <cnsasumi@gmail.com>
2023-06-05 19:13:43 +0800

    upd

--- src/http.php
+++ src/http.php
@@ -113,15 +113,15 @@ function http_get_request_headers(){
 			$headers[str_replace('_', '-', substr($key, 5))] = $value;
 		}
 		if(isset($_SERVER['PHP_AUTH_DIGEST'])){
-			$header['AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
+			$headers['AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
 		}elseif(isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])){
-			$header['AUTHORIZATION'] = base64_encode($_SERVER['PHP_AUTH_USER'].':'.$_SERVER['PHP_AUTH_PW']);
+			$headers['AUTHORIZATION'] = base64_encode($_SERVER['PHP_AUTH_USER'].':'.$_SERVER['PHP_AUTH_PW']);
 		}
 		if(isset($_SERVER['CONTENT_LENGTH'])){
-			$header['CONTENT-LENGTH'] = $_SERVER['CONTENT_LENGTH'];
+			$headers['CONTENT-LENGTH'] = $_SERVER['CONTENT_LENGTH'];
 		}
 		if(isset($_SERVER['CONTENT_TYPE'])){
-			$header['CONTENT-TYPE'] = $_SERVER['CONTENT_TYPE'];
+			$headers['CONTENT-TYPE'] = $_SERVER['CONTENT_TYPE'];
 		}
 	}
 	return $headers;
