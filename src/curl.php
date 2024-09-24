8fddde196039965e052dfdc39cd47531bc11e6d6
sasumi <cnsasumi@gmail.com>
2024-09-24 14:53:24 +0800

    upd

--- src/curl.php
+++ src/curl.php
@@ -50,6 +50,26 @@ function curl_get($url, $data = null, array $curl_option = []){
 }
 
 /**
+ * JSON方式发送GET请求
+ * @param string $url
+ * @param mixed $data
+ * @param array $curl_option
+ * @return array
+ * @throws \Exception
+ */
+function curl_get_json($url, $data = null, array $curl_option = []){
+	if($data){
+		$url .= (strpos($url, '?') !== false ? '&' : '?').curl_data2str($data);
+	}
+	return curl_query($url, array_merge_assoc([
+		CURLOPT_HTTPHEADER => [
+			'Content-Type: application/json; charset=utf-8',
+			'Content-Length: '.strlen($data),
+		],
+	], $curl_option, true));
+}
+
+/**
  * POST请求
  * @param string $url
  * @param mixed|null $data
