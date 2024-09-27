1df4d83dfb4c61efa8a362b3c5da1ad39c358a20
sasumi <cnsasumi@gmail.com>
2024-09-27 20:07:13 +0800

    upd

--- src/curl.php
+++ src/curl.php
@@ -34,6 +34,96 @@ const CURLOPT_FOLLOWING_COOKIE_FILE = __NAMESPACE__.'/CURLOPT_FOLLOWING_COOKIE_F
 //自动修复html中相对路径
 const CURLOPT_HTML_FIX_RELATIVE_PATH = __NAMESPACE__.'/CURLOPT_HTML_FIX_RELATIVE_PATH';
 
+//curl错误消息映射
+const CURL_ERROR_MAP = [
+	1  => 'CURLE_UNSUPPORTED_PROTOCOL',
+	2  => 'CURLE_FAILED_INIT',
+	3  => 'CURLE_URL_MALFORMAT',
+	4  => 'CURLE_URL_MALFORMAT_USER',
+	5  => 'CURLE_COULDNT_RESOLVE_PROXY',
+	6  => 'CURLE_COULDNT_RESOLVE_HOST',
+	7  => 'CURLE_COULDNT_CONNECT',
+	8  => 'CURLE_FTP_WEIRD_SERVER_REPLY',
+	9  => 'CURLE_REMOTE_ACCESS_DENIED',
+	11 => 'CURLE_FTP_WEIRD_PASS_REPLY',
+	13 => 'CURLE_FTP_WEIRD_PASV_REPLY',
+	14 => 'CURLE_FTP_WEIRD_227_FORMAT',
+	15 => 'CURLE_FTP_CANT_GET_HOST',
+	17 => 'CURLE_FTP_COULDNT_SET_TYPE',
+	18 => 'CURLE_PARTIAL_FILE',
+	19 => 'CURLE_FTP_COULDNT_RETR_FILE',
+	21 => 'CURLE_QUOTE_ERROR',
+	22 => 'CURLE_HTTP_RETURNED_ERROR',
+	23 => 'CURLE_WRITE_ERROR',
+	25 => 'CURLE_UPLOAD_FAILED',
+	26 => 'CURLE_READ_ERROR',
+	27 => 'CURLE_OUT_OF_MEMORY',
+	28 => 'CURLE_OPERATION_TIMEDOUT',
+	30 => 'CURLE_FTP_PORT_FAILED',
+	31 => 'CURLE_FTP_COULDNT_USE_REST',
+	33 => 'CURLE_RANGE_ERROR',
+	34 => 'CURLE_HTTP_POST_ERROR',
+	35 => 'CURLE_SSL_CONNECT_ERROR',
+	36 => 'CURLE_BAD_DOWNLOAD_RESUME',
+	37 => 'CURLE_FILE_COULDNT_READ_FILE',
+	38 => 'CURLE_LDAP_CANNOT_BIND',
+	39 => 'CURLE_LDAP_SEARCH_FAILED',
+	41 => 'CURLE_FUNCTION_NOT_FOUND',
+	42 => 'CURLE_ABORTED_BY_CALLBACK',
+	43 => 'CURLE_BAD_FUNCTION_ARGUMENT',
+	45 => 'CURLE_INTERFACE_FAILED',
+	47 => 'CURLE_TOO_MANY_REDIRECTS',
+	48 => 'CURLE_UNKNOWN_TELNET_OPTION',
+	49 => 'CURLE_TELNET_OPTION_SYNTAX',
+	51 => 'CURLE_PEER_FAILED_VERIFICATION',
+	52 => 'CURLE_GOT_NOTHING',
+	53 => 'CURLE_SSL_ENGINE_NOTFOUND',
+	54 => 'CURLE_SSL_ENGINE_SETFAILED',
+	55 => 'CURLE_SEND_ERROR',
+	56 => 'CURLE_RECV_ERROR',
+	58 => 'CURLE_SSL_CERTPROBLEM',
+	59 => 'CURLE_SSL_CIPHER',
+	60 => 'CURLE_SSL_CACERT',
+	61 => 'CURLE_BAD_CONTENT_ENCODING',
+	62 => 'CURLE_LDAP_INVALID_URL',
+	63 => 'CURLE_FILESIZE_EXCEEDED',
+	64 => 'CURLE_USE_SSL_FAILED',
+	65 => 'CURLE_SEND_FAIL_REWIND',
+	66 => 'CURLE_SSL_ENGINE_INITFAILED',
+	67 => 'CURLE_LOGIN_DENIED',
+	68 => 'CURLE_TFTP_NOTFOUND',
+	69 => 'CURLE_TFTP_PERM',
+	70 => 'CURLE_REMOTE_DISK_FULL',
+	71 => 'CURLE_TFTP_ILLEGAL',
+	72 => 'CURLE_TFTP_UNKNOWNID',
+	73 => 'CURLE_REMOTE_FILE_EXISTS',
+	74 => 'CURLE_TFTP_NOSUCHUSER',
+	75 => 'CURLE_CONV_FAILED',
+	76 => 'CURLE_CONV_REQD',
+	77 => 'CURLE_SSL_CACERT_BADFILE',
+	78 => 'CURLE_REMOTE_FILE_NOT_FOUND',
+	79 => 'CURLE_SSH',
+	80 => 'CURLE_SSL_SHUTDOWN_FAILED',
+	81 => 'CURLE_AGAIN',
+	82 => 'CURLE_SSL_CRL_BADFILE',
+	83 => 'CURLE_SSL_ISSUER_ERROR',
+	84 => 'CURLE_FTP_PRET_FAILED',
+	85 => 'CURLE_RTSP_CSEQ_ERROR',
+	86 => 'CURLE_RTSP_SESSION_ERROR',
+	87 => 'CURLE_FTP_BAD_FILE_LIST',
+	88 => 'CURLE_CHUNK_FAILED',
+	89 => "CURLE_NO_CONNECTION_AVAILABLE", // No connection available, the session will be queued
+	90 => "CURLE_SSL_PINNEDPUBKEYNOTMATCH", // specified pinned public key did not  match
+	91 => "CURLE_SSL_INVALIDCERTSTATUS", // invalid certificate status
+	92 => "CURLE_HTTP2_STREAM", // stream error in HTTP/2 framing layer
+	93 => "CURLE_RECURSIVE_API_CALL", // an api function was called from inside a callback
+	94 => "CURLE_AUTH_ERROR", // an authentication function returned an error
+	95 => "CURLE_HTTP3", // An HTTP/3 layer problem
+	96 => "CURLE_QUIC_CONNECT_ERROR", // QUIC connection error
+	97 => "CURLE_PROXY", // proxy handshake error
+	98 => "CURLE_SSL_CLIENTCERT", // client-side certificate required
+];
+
 /**
  * CURL GET请求
  * @param string $url
@@ -85,7 +175,7 @@ function curl_post_json($url, array $data = [], array $curl_option = []){
 /**
  * curl post 提交文件
  * @param string $url
- * @param array $file_map [filename=>file,...] 文件名称映射
+ * @param array $file_map [filename=>file, filename=>[file, mime]...] 文件名称映射，这里如果不提供mime信息，后端接收到的可能是 application/octet-stream
  * @param mixed $ext_param 同时提交的其他post参数
  * @param array $curl_option curl选项
  * @return array curl_query返回结果，包含 [info=>[], head=>'', body=>''] 信息
@@ -93,10 +183,13 @@ function curl_post_json($url, array $data = [], array $curl_option = []){
  */
 function curl_post_file($url, array $file_map, array $ext_param = [], array $curl_option = []){
 	foreach($file_map as $name => $file){
+		$mime = '';
+		if(is_array($file)){
+			[$file, $mime] = $file;
+		}
 		if(!is_file($file)){
 			throw new Exception('file no found:'.$file);
 		}
-		$mime = mime_content_type($file);
 		$ext_param[$name] = curl_file_create($file, $mime);
 	}
 	return curl_query($url, array_merge_assoc([
@@ -148,10 +241,9 @@ function curl_query($url, array $curl_option){
 
 	$ret = [];
 	$ret['info'] = curl_getinfo($ch);
-	$error = curl_error($ch);
-	if($error){
-		$errno = curl_errno($ch);
-		$ret['error'] = "Curl Error($errno) $error";
+	$errno = curl_errno($ch);
+	if($errno){
+		$ret['error'] = curl_error_message($errno);
 	}else{
 		[$ret['head'], $ret['body']] = curl_cut_raw($ch, $raw_string);
 		if(isset($curl_option[CURLOPT_PAGE_ENCODING])){
@@ -163,7 +255,6 @@ function curl_query($url, array $curl_option){
 			$ret['body'] = html_fix_relative_path($ret['body'], $ret['info']['url']);
 		}
 	}
-
 	curl_close($ch);
 	return $ret;
 }
@@ -516,6 +607,18 @@ function curl_cut_raw($ch, $raw_string){
 	$head = substr($raw_string, 0, $header_size);
 	$body = substr($raw_string, $header_size);
 	return [$head, $body];
+}
+
+/**
+ * 获取curl错误信息
+ * @param int $error_no
+ * @return string 为空表示成功
+ */
+function curl_error_message($error_no){
+	if(!$error_no){
+		return '';
+	}
+	return CURL_ERROR_MAP[$error_no] ?: "curl error happens($error_no).";
 }
 
 /**
